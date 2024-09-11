edb92e0c0e3d9cd957ddbf4636a78955102f207f
mradulnatani <mradulnatni0@gmail.com>
2024-09-11 23:19:17 +0530

    Co-authored-by: Rudraksh-K12 <Rudraksh-K12@users.noreply.github.com>

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div className='bg-black h-[70vh] flex items-center justify-center px-11 py-10'>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "~/components/ui/navigation-menu"
+
+const navbar = () => {
+  return (
+    <div>
+      <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+
+    </div>
+  )
+}
+
+export default navbar
