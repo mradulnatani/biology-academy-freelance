892a6e86092b51a83fcfff9ad6e4d2d2d3841280
mradulnatani <mradulnatni0@gmail.com>
2024-09-14 01:42:56 +0530

    bg changed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -85,28 +85,28 @@ export function Home() {
       {/* Statistics Section */}
       <section id="statistics" className="flex justify-center w-full py-10 pt-44">
         <div className="w-[70vw] grid grid-cols-1 md:grid-cols-4 gap-4">
-          <Card className="bg-green-700 text-white text-center p-4 hover:bg-green-600">
+          <Card className="bg-green-800 text-white text-center p-4 hover:bg-green-600">
             <CardContent>
               <h2 className="text-2xl font-bold">3K+</h2>
               <p className="mt-2 text-lg">Students Taught</p>
             </CardContent>
           </Card>
 
-          <Card className="bg-green-700 text-white text-center p-4 hover:bg-green-600">
+          <Card className="bg-green-800 text-white text-center p-4 hover:bg-green-600">
             <CardContent>
               <h2 className="text-2xl font-bold">20+</h2>
               <p className="mt-2 text-lg">Years of Experience</p>
             </CardContent>
           </Card>
 
-          <Card className="bg-green-700 text-white text-center p-4 hover:bg-green-600">
+          <Card className="bg-green-800 text-white text-center p-4 hover:bg-green-600">
             <CardContent>
               <h2 className="text-2xl font-bold">2.5K</h2>
               <p className="mt-2 text-lg">Qualified Students</p>
             </CardContent>
           </Card>
 
-          <Card className="bg-green-700 text-white text-center p-4 hover:bg-green-600">
+          <Card className="bg-green-800 text-white text-center p-4 hover:bg-green-600">
             <CardContent>
               <h2 className="text-2xl font-bold">2.5K</h2>
               <p className="mt-2 text-lg">Aims Students</p>
